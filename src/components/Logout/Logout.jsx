b6cacdc5994ceca48a1d017ffa8eb502756aa312
shubh <shubhverma2017@gamil.com>
2022-08-01 14:09:52 +0530

    firstf

--- src/components/Logout/Logout.jsx
+++ src/components/Logout/Logout.jsx
@@ -2,21 +2,34 @@ import axios from "axios";
 import React,{useContext, useEffect} from "react";
 import { useNavigate } from "react-router-dom";
 import { UserContext } from "../../App";
-import Cookies from 'universal-cookie';
 axios.defaults.withCredentials = true
 const Logout=()=>{
     const Navigate=useNavigate();
     const {state,dispatch}=useContext(UserContext);
-    const deleteCookie = () => {
-        axios.get('https://fymoo.herokuapp.com/deleteCookie',{ withCredentials: true }).then((res) =>{
-            dispatch({type:"USER",payload:false});
-             Navigate('/');
-          console.log(res.data)
-        })
-      }
+    // const deleteCookie = () => {
+    //     axios.get('https://fymoo.herokuapp.com/deleteCookie',{ withCredentials: true }).then((res) =>{
+    //         dispatch({type:"USER",payload:false});
+    //          Navigate('/');
+    //       console.log(res.data)
+    //     })
+    //   }
     const calllogout=async()=>{
         try{
-            deleteCookie();
+            // deleteCookie();
+            const config = {
+                headers: {
+                    
+                  'Content-Type': 'application/json',
+                },
+              };
+            
+            const data=await axios.get('https://fymoo.herokuapp.com/deleteCookie',config);
+            if(data.status===200){
+                dispatch({type:"USER",payload:false});
+                Navigate('/');
+            }
+            //  createCookie();
+         
              
 
         }catch(err){
