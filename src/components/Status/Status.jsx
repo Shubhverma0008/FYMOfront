43ad78da7f12ae16b3c8a42acaf54820250340fc
shubh <shubhverma2017@gamil.com>
2022-08-02 23:01:47 +0530

    firstf

--- src/components/Status/Status.jsx
+++ src/components/Status/Status.jsx
@@ -9,12 +9,13 @@ const Status=()=>{
  const {state,dispatch}= useContext(UserContext);
  const [data,setData]=useState([]);
   const Navigate=useNavigate();
-  var user;
+  var check;
   const callstatuspage= async()=>{
     try{
-       user= await axios.get('https://fymoo.herokuapp.com/api/auth/getdata');
+      const user= await axios.get('https://fymoo.herokuapp.com/api/auth/getdata');
       dispatch({type:"USER",payload:true})
       // console.log(user);
+      check=user.data.phone;
       const list=await axios.get(`https://fymoo.herokuapp.com/api/auth/getmissingpersonlist/${user.data._id}`);
       setData(list.data);  
     }catch(err){
@@ -28,7 +29,7 @@ const Status=()=>{
     return (
         <>
         <div className="row row1">
-    { user.data.phone==9616543935  ?<> <Post key="62e797d1e6466f1e88f764bd" name="Shubh" height="169" location="MMB" currentstatus="3" img="/assets/shubhPic.jpg"/>
+    { check===9616543935  ?<> <Post key="62e797d1e6466f1e88f764bd" name="Shubh" height="169" location="MMB" currentstatus="3" img="/assets/shubhPic.jpg"/>
         <Post key="62e797d1e6466f1e88f764bd" name="John" height="179" location="USA" currentstatus="2" img="/assets/man1.jpg"/>
         <Post key="62e797d1e6466f1e88f764bd" name="Jolly" height="165" location="LONDON" currentstatus="1" img="/assets/girl.jpg"/>
         <Post key="62e797d1e6466f1e88f764bd" name="Kaby" height="179" location="New York" currentstatus="1" img="/assets/man2.jpg"/>
