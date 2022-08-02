18a090508f8624b00dcaef4b3bf6a074516656fd
shubh <shubhverma2017@gamil.com>
2022-08-02 23:17:46 +0530

    firstf

--- src/components/Status/Status.jsx
+++ src/components/Status/Status.jsx
@@ -30,7 +30,8 @@ const Status=()=>{
     return (
         <>
         <div className="row row1">
-    { check==="62e797d1e6466f1e88f764bd" ?<> <Post key="62e797d1e6466f1e88f764bd" name="Shubh" height="169" location="MMB" currentstatus="3" img="/assets/shubhPic.jpg"/>
+    { console.log(check)}
+     { check==="62e797d1e6466f1e88f764bd"?<> <Post key="62e797d1e6466f1e88f764bd" name="Shubh" height="169" location="MMB" currentstatus="3" img="/assets/shubhPic.jpg"/>
         <Post key="62e797d1e6466f1e88f764bd" name="John" height="179" location="USA" currentstatus="2" img="/assets/man1.jpg"/>
         <Post key="62e797d1e6466f1e88f764bd" name="Jolly" height="165" location="LONDON" currentstatus="1" img="/assets/girl.jpg"/>
         <Post key="62e797d1e6466f1e88f764bd" name="Kaby" height="179" location="New York" currentstatus="1" img="/assets/man2.jpg"/>
